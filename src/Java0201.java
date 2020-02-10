991a4c459607ff602ee67fae73d249869a82fa36
liyuhuan123 <2502268134@qq.com>
2020-02-10 15:32:27 +0800

    2月10日代码练习

--- src/Java0201.java
+++ src/Java0201.java
@@ -1,19 +1,20 @@
-public class Java0201 {
-    public  static String toLowerCase(String sc){
-        char[] scs = sc.toCharArray();
-        for(int i = 0;i<scs.length;i++){
-            if(scs[i]>='A'&&scs[i]<='Z'){
-                scs[i]+=32;
-            }else if(scs[i]>='a'&&scs[i]<='z'){
-                scs[i]-=32;
-            }
-        }
-        String str = String.valueOf(scs);
-        return str;
+public class Java0201{
+    private String name;//实例成员变量
+    private int age;
+    private String sex;
+    //默认构造函数   构造对象
+    public Java0201(){
+        this.name = "caocao";
+        this.age = 10;
+        this.sex = "男";
     }
-public static void main(String[] args){
-    String sc = "addscskUU";
-    System.out.println(toLowerCase(sc));
+    //带有三个参数的构造函数
+    public Java0201(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+    public void show(){
+        System.out.println("name:"+name+"age:"+age+"sex:"+sex);
+    }
 }
-
-    }
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,9 @@
-public class Test {
+public class Test{
+    public static void main(String[] args){
+        Java0201 Java = new Java0201();//调用不带参数的构造函数 如果程序没有提供会调用不带参数的构造函数
+                Java.show();
+        Java0201 Java1 = new Java0201("张飞",80,"男");
+        Java1.show();
+    }
 
 }
