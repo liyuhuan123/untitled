23fa945c7bdf74ca7a0ca59e6a5ca9a9a5d059a3
liyuhuan123 <2502268134@qq.com>
2020-02-12 13:53:29 +0800

    2月12日代码练习

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,29 @@
 public class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+    public boolean isLongPressedName(String name, String typed) {
+        char[] na = name.toCharArray();//字符串转化为字符数组
+        char[] ty = typed.toCharArray();//字符串转化为字符数组
+        int i = 0;
+        int j = 0;
+        if(na.length>ty.length){
+            return false;
+        }
+        while(i<na.length&&j<ty.length){
+            if(na[i] == ty[j]){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+            if(i == na.length){
+                return true;
             }
         }
         return false;
     }
     public static void main(String[] args){
-        int[] nums = {1,2,3,41};
+        String name = "xiaoli";
+        String typed = "xiaol";
         Solution Solution = new Solution();
-        System.out.println(Solution.containsDuplicate(nums));
+        System.out.println(Solution.isLongPressedName(name,typed));
     }
 }
