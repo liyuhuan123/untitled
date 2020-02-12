429c279e745f7a68d5c0bf339ce894aeec252e02
liyuhuan123 <2502268134@qq.com>
2020-02-12 16:19:37 +0800

    2月12日代码练习

--- src/Solution/Solution.java
+++ src/Solution/Solution.java
@@ -1,32 +1,22 @@
 package Solution;
 
 public class Solution {
-    public int[] sortArrayByParity(int[] A) {
-        int left = 0;
-        int right = A.length-1;
-        while(left<right){
-            if(A[left]%2 != 0){
-                if(A[right]%2 ==0){
-                    int tmp = A[left];
-                    A[left] = A[right];
-                    A[right] = tmp;
-                    left++;
-                    right--;
-                }else{
-                    right--;
-                }
-            }else{
-                left++;
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        int sum1 = 0;
+        for (int x: nums)
+            sum += x;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum1 == sum - sum1 - nums[i]){
+                return i;}else{
+                sum1 += nums[i];
             }
         }
-    return A;
+        return -1;
     }
     public static void main(String[] args){
-        int[] A = {1,2,3,4};
+        int[] arr = {1,2,34,5,6,7};
         Solution Solution = new Solution();
-        Solution.sortArrayByParity(A);
-        for(int i = 0;i<A.length;i++){
-            System.out.print(A[i]+" ");
-        }
+        System.out.println(Solution.pivotIndex(arr));
     }
 }
