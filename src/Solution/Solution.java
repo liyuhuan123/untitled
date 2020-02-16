0dea3b164af60efd916ac95b223afa111124440e
liyuhuan123 <2502268134@qq.com>
2020-02-16 15:17:47 +0800

    2月16日代码练习

--- src/Solution/Solution.java
+++ src/Solution/Solution.java
@@ -1,17 +1,17 @@
 package Solution;
-
+import java.util.Arrays;
 public class Solution {
-    public int[] plusOne(int[] digits) {
-        int ch = digits.length;
-        for(int i = ch - 1; i >= 0; i--) {
-            digits[i]++;
-            digits[i] %= 10;
-            if(digits[i]!=0)
-                return digits;
-        }
-        digits = new int[ch + 1];
-        digits[0] = 1;
-        return digits;
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length<3)
+            return nums[nums.length-1];
+        else
+            return nums[nums.length-3];
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,54};
+        Solution Solution = new Solution();
+        System.out.println(Solution.thirdMax(arr));
     }
 }
 
