24dc50c656d9750e60851f150295bf6e496fb8de
liyuhuan123 <2502268134@qq.com>
2020-02-16 14:28:28 +0800

    2月16日代码练习

--- src/Solution/Solution.java
+++ src/Solution/Solution.java
@@ -1,22 +1,17 @@
 package Solution;
 
 public class Solution {
-    public int pivotIndex(int[] nums) {
-        int sum = 0;
-        int sum1 = 0;
-        for (int x: nums)
-            sum += x;
-        for (int i = 0; i < nums.length; i++) {
-            if (sum1 == sum - sum1 - nums[i]){
-                return i;}else{
-                sum1 += nums[i];
-            }
+    public int[] plusOne(int[] digits) {
+        int ch = digits.length;
+        for(int i = ch - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] %= 10;
+            if(digits[i]!=0)
+                return digits;
         }
-        return -1;
-    }
-    public static void main(String[] args){
-        int[] arr = {1,2,34,5,6,7};
-        Solution Solution = new Solution();
-        System.out.println(Solution.pivotIndex(arr));
+        digits = new int[ch + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
+
