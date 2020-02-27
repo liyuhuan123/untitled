56b5da9e6df1dd38999d17a68f12e102f3876a93
liyuhuan123 <2502268134@qq.com>
2020-02-27 18:04:35 +0800

    2月27日代码练习

--- src/Solution/Solution.java
+++ src/Solution/Solution.java
@@ -35,5 +35,67 @@ public class Solution{
             }
             return head;
         }
+        public ListNode reverseList(ListNode head) {
+            if(head == null){
+                //当前链表为空，不需要逆置
+                return null;
+            }
+            if(head.next == null){
+                //链表中只有一个元素，也不需要逆置
+                return head;
+            }
+            ListNode prev = head;
+            ListNode cur = null;
+            ListNode newHead = null;
+            while(prev != null){
+                ListNode next = prev.next;
+                if(next == null){
+                    //当前prev就是原链表的末尾
+                    newHead = prev;
+                }
+                //逆置的核心
+                prev.next = cur;
+                cur = prev;
+                prev = next;
+            }
+            return newHead;
+        }
+        //给定一个带有头结点head的非空单链表，返回链表的中间结点，如果有两个中间结点
+        //则返回第二个中间结点
+        public ListNode middleNode(ListNode head) {
+            int steps = size(head)/2;
+            ListNode cur = head;
+            for(int i = 0;i<steps;i++){
+                cur = cur.next;
+            }
+            return cur;
+        }
+        public int size(ListNode head){
+            int size = 0;
+            ListNode cur = head;
+            while(cur != null){
+                size++;
+                cur = cur.next;
+            }
+            return size;
+        }
+        //输入一个链表，输出该链表中倒数第k个结点
+        public ListNode FindKthToTail(ListNode head,int k) {
+            int size = size(head);
+            if (head == null) {
+                //链表为空，不合法
+                return null;
+            }
+            if (k <= 0 || k > size) {
+                //移动步数小于0或者移动步数大于链表长度，也不合法
+                return null;
+            }
+            ListNode cur = head;
+            int steps = size - k;
+            for (int i = 0; i < steps; i++) {
+                cur = cur.next;
+            }
+            return cur;
+        }
     }
 }
