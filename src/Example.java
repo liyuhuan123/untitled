055095e33d315a6f46dca0390391347336dc7980
liyuhuan123 <2502268134@qq.com>
2020-03-13 22:22:11 +0800

    3月13日代码练习

--- src/Example.java
+++ src/Example.java
@@ -1,7 +1,6 @@
 public class Example {
         public static void main(String args[]){
            String str = "helloworld";
-           System.out.println(str.replaceAll("l","m"));
-           System.out.println(str.replaceFirst("l","m"));
+           System.out.println(str.contains("world"));
         }
 }
