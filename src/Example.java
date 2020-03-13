3e6c61013c5bff7e7c336415b103719b5f7e99b9
liyuhuan123 <2502268134@qq.com>
2020-03-13 22:18:46 +0800

    3月13日代码练习

--- src/Example.java
+++ src/Example.java
@@ -1,14 +1,7 @@
 public class Example {
-        String str = new String("good");
-        char[ ] ch = { 'a' , 'b' , 'c' };
         public static void main(String args[]){
-            Example ex = new Example();
-            ex.change(ex.str,ex.ch);
-            System.out.print(ex.str + " and ");
-            System.out.print(ex.ch);
-        }
-        public void change(String str,char ch[ ]){
-            str = "test ok";
-            ch[0] = 'g';
+           String str = "helloworld";
+           System.out.println(str.replaceAll("l","m"));
+           System.out.println(str.replaceFirst("l","m"));
         }
 }
