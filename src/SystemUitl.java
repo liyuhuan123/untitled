8df696ab7d6183bbda811f10b4d2870910d82151
liyuhuan123 <2502268134@qq.com>
2020-03-13 22:06:51 +0800

    3月13日代码练习

---
+++ src/SystemUitl.java
@@ -0,0 +1,8 @@
+public class SystemUitl {
+        public static boolean isAdmin(String userId){
+            return userId.toLowerCase()=="admin";
+        }
+        public static void main(String[] args){
+            System.out.println(isAdmin("Admin"));
+    }
+}
