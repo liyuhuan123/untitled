7e5ede7b77e5bea312dd3be33fc98c2376ac472f
liyuhuan123 <2502268134@qq.com>
2020-03-15 17:45:48 +0800

    3月15日代码练习

--- src/Example.java
+++ src/Example.java
@@ -1,9 +1,11 @@
 public class Example {
     //给定一个字符串，判断其是否由全部数字所组成
     public static void main(String[] args){
-        String str1 = "hello";
-        String str2 = "Hello";
-        System.out.println(str1.equals(str2));//false
-        System.out.println(str1.equalsIgnoreCase(str2));//true
+        //观察compareTo比较
+        System.out.println("A".compareTo("a"));
+        System.out.println("a".compareTo("A"));
+        System.out.println("A".compareTo("A"));
+        System.out.println("AB".compareTo("AC"));
+        System.out.println("刘".compareTo("杨"));
     }
 }
