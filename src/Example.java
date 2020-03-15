c82d5632fe99cfb02ed0bdedf0fe9d9c35807891
liyuhuan123 <2502268134@qq.com>
2020-03-15 17:35:29 +0800

    3月15日代码练习

--- src/Example.java
+++ src/Example.java
@@ -1,17 +1,9 @@
 public class Example {
     //给定一个字符串，判断其是否由全部数字所组成
     public static void main(String[] args){
-        String str = "1a3456";
-        System.out.println(isNumber(str)?"字符串由数字所构成！":
-                "字符串中有非数字成员！");
-    }
-    public static boolean isNumber(String str){
-        char[] data = str.toCharArray();
-        for(int i = 0;i < data.length;i++){
-            if(data[i] < '0' || data[i] > '9'){
-                return false;
-            }
-        }
-        return true;
+        String str1 = "hello";
+        String str2 = "Hello";
+        System.out.println(str1.equals(str2));//false
+        System.out.println(str1.equalsIgnoreCase(str2));//true
     }
 }
