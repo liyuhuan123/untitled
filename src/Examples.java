a1fc5c0a4fb3203056c24a2f2b11f3953f96c520
liyuhuan123 <2502268134@qq.com>
2020-03-16 16:19:50 +0800

    3月16日代码练习

--- src/Examples.java
+++ src/Examples.java
@@ -1,15 +1,36 @@
 public class Examples {
     public static void main(String[] args) {
-       //实现字符串的拆分处理
-        String str = "hello world hello world";//按照空格拆分
-        String[] result = str.split(" ");
-        for(String s : result){
-            System.out.println(s);
+       //观察字符串的截取
+        String str = "helloworld";
+        System.out.println(str.substring(5));
+        System.out.println(str.substring(0,5));
+        //观察trim方法的使用
+        String str1 = "  hello world  ";
+        System.out.println("["+str1+"]");
+        System.out.println("["+str1.trim()+"]");
+        //大小写转换
+        String str2 = "hello@&%*&^%world哈哈";
+        System.out.println(str2.toUpperCase());
+        System.out.println(str2.toLowerCase());
+        //字符串length()
+        String str3 = "  hello ^%$3@87((world哈哈哈";
+        System.out.println(str3.length());
+        //观察isEmpty方法
+        System.out.println("hello".isEmpty());
+        System.out.println("".isEmpty());
+        System.out.println(new String().isEmpty());
+        //首字母大写
+        System.out.println(fistUpper("yuisama"));
+        System.out.println(fistUpper(""));
+        System.out.println(fistUpper("a"));
         }
-        String str1 = "192.168.1.1";
-        String[] results = str1.split("\\.");
-        for(String s : results){
-            System.out.println(s);
+        public static String fistUpper(String str){
+        if("".equals(str)||str == null){
+            return str;
         }
-    }
+        if(str.length() > 1){
+            return str.substring(0,1).toUpperCase()+str.substring(1);
+        }
+        return str.toUpperCase();
+        }
 }
