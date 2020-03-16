e0dc6db8bf40ae6ecabd799d696bfea26ffb53f6
liyuhuan123 <2502268134@qq.com>
2020-03-16 15:20:10 +0800

    3月16日代码练习

---
+++ src/Examples.java
@@ -0,0 +1,7 @@
+public class Examples {
+    public static void main(String[] args) {
+        String str = "helloworld";
+        System.out.println(str.replaceAll("l","*"));
+        System.out.println(str.replaceFirst("l","*"));
+    }
+}
