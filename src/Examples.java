dc623ad4078af8f064bb6cef1638317d2850c8ac
liyuhuan123 <2502268134@qq.com>
2020-03-16 15:45:44 +0800

    3月16日代码练习

--- src/Examples.java
+++ src/Examples.java
@@ -1,7 +1,15 @@
 public class Examples {
     public static void main(String[] args) {
-        String str = "helloworld";
-        System.out.println(str.replaceAll("l","*"));
-        System.out.println(str.replaceFirst("l","*"));
+       //实现字符串的拆分处理
+        String str = "hello world hello world";//按照空格拆分
+        String[] result = str.split(" ");
+        for(String s : result){
+            System.out.println(s);
+        }
+        String str1 = "192.168.1.1";
+        String[] results = str1.split("\\.");
+        for(String s : results){
+            System.out.println(s);
+        }
     }
 }
