f30284ceb5b24139a0f06392784da13acf5afc04
liyuhuan123 <2502268134@qq.com>
2020-03-17 15:24:00 +0800

    3月17日代码练习

--- src/Java0317/Main.java
+++ src/Java0317/Main.java
@@ -2,17 +2,10 @@ package Java0317;
 
 public class Main {
     public static void main(String[] args){
-        //产生对象 实例化对象
-        Person person = new Person();//person为对象的引用
-        System.out.println(person.name);
-        System.out.println(person.age);
-        //System.out.println(person.count);//会有警告
-        //正确访问方式
-        System.out.println(Person.count);
-        System.out.println(Person.COUNT);
-        Person.staticTest();
-        //总结：所有被static所修饰的方法或者属性，全部不依赖于对象
-        person.eat();
-        person.sleep();
+        Person person = new Person();
+        person.setName("caocao");
+        String name = person.getName();
+        System.out.println(name);
+        person.show();
     }
 }
--- src/Java0317/Person.java
+++ src/Java0317/Person.java
@@ -1,24 +1,19 @@
 package Java0317;
 
 public class Person {
-    public int age;//实例变量，存放在对象内
-    public String name;//实例变量
-    public String sex;//实例变量
-    public static int count;//类变量，属于类本身，存在 方法区
-    public final int size = 10;//fianl修饰的叫常量，后续不可更改
-    public static final int COUNT = 99;//静态常量
-    //成员实例函数
-    public void eat(){
-        System.out.println("eat!");
+    private String name = "张三";
+    private int age = 18;
+
+    public String getName() {
+        return name;
     }
-    public void sleep(){
-        System.out.println("sleep!");
+
+    public void setName(String name) {
+        this.name = name;
     }
-    //静态成员函数
-    public static void staticTest(){
-        //不能访问非静态成员
-        //sex = "man";error
-        System.out.println("StaticTest()");
+
+    public void show(){
+        System.out.println("我叫"+name+",今年"+age+"岁");
     }
 
 }
