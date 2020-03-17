aa3424b172a3f1c44e7c6c684a4fa6ae907881d1
liyuhuan123 <2502268134@qq.com>
2020-03-17 14:52:47 +0800

    3月17日代码练习

--- src/Java0317/Main.java
+++ src/Java0317/Main.java
@@ -2,16 +2,7 @@ package Java0317;
 
 public class Main {
     public static void main(String[] args){
-        TestDemo t1 = new TestDemo();
-        t1.a++;
-        TestDemo.count++;
-        System.out.println(t1.a);
-        System.out.println(TestDemo.count);
-        System.out.println("=============");
-        TestDemo t2 = new TestDemo();
-        t2.a++;
-        TestDemo.count++;
-        System.out.println(t2.a);
+        TestDemo.change();//无需创建实例对象就可以调用
         System.out.println(TestDemo.count);
     }
 }
--- src/Java0317/TestDemo.java
+++ src/Java0317/TestDemo.java
@@ -3,4 +3,8 @@ package Java0317;
 public class TestDemo {
     public int a;
     public static int count;
+    public static void change(){
+        count = 100;
+        //a = 10;不能访问非静态数据成员
+    }
 }
