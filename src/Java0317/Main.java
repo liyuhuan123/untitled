8abc50f68d3e7aec16936c92fd388f766e95a15a
liyuhuan123 <2502268134@qq.com>
2020-03-19 13:31:17 +0800

    3月19日代码练习

--- src/Java0317/Main.java
+++ src/Java0317/Main.java
@@ -2,9 +2,8 @@ package Java0317;
 
 public class Main {
     public static void main(String[] args){
-        Person p1 = new Person();//调用不带参数的构造函数,如果程序没有提供会调用不带参数的构造函数
-        p1.show();
-        Person p2 = new Person("张飞",80,"男");
-        p2.show();
+        //直接使用{}定义，普通方法块
+        int x = 10;
+        System.out.println("x1 = "+x);
     }
 }
