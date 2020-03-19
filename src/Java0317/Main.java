98cc4ec5fbd63e541b56c0ea4760a893242c377e
liyuhuan123 <2502268134@qq.com>
2020-03-19 14:01:54 +0800

    3月19日代码练习

--- src/Java0317/Main.java
+++ src/Java0317/Main.java
@@ -3,6 +3,6 @@ package Java0317;
 public class Main {
     public static void main(String[] args){
         Person p1 = new Person();
-        p1.show();
+        Person p2 = new Person();
     }
 }
